2c0c84e2b61af0d9edbbe4ca46db814b065e33fd
BotNet64 <zagres.1512@gmail.com>
2025-08-28 20:18:25 +0330

    Fix styling in types.py and strategies/__init__.py
    Fix the styling in common/types.py and strategies/__init__.py
    Limit lines to 80 characters long and wrap them,
    as well as correct the import order in strategies/__init__.py

--- strategies/__init__.py
+++ strategies/__init__.py
@@ -1,28 +1,32 @@
 from abc import ABC, abstractmethod
+from typing import Tuple
+
 from common.types import RoundInfo
-from typing import Optional, Tuple
 
 
 class Strategy(ABC):
     @abstractmethod
     def play(
-            self,
-            last_moves: Optional[Tuple[bool, bool]],
-            round_info: RoundInfo
+        self,
+        last_moves: Tuple[bool, bool] | Tuple[()],
+        round_info: RoundInfo
     ) -> bool:
         """
         Abstract base class for strategies.
 
-        Extend this class and implement the play() method to define your strategy.
+        Extend this class and implement the play() method to define your
+        strategy.
         Place your .py file in the 'strategies' folder for auto-loading.
 
         Usage tip: Use last_moves and round_info to decide your move.
         Store any persistent state inside self if needed.
 
         Args:
-            last_moves (Optional[Tuple[bool, bool]]): A tuple of (my_last_move, opponent_last_move).
+            last_moves (Tuple[bool, bool] | Tuple[()]):
+                A tuple of (my_last_move, opponent_last_move).
                 None if this is the first round.
-            round_info (RoundInfo): Information about the current round provided by the engine.
+            round_info (RoundInfo):
+                Information about the current round provided by the engine.
 
         Returns:
             bool: True for cooperate, False for defect.
