1293819dba1ee5466dfaebafadc91d4090c3359f
BotNet64 <148477473+BotNet6420@users.noreply.github.com>
2025-09-01 16:37:23 +0330

    Implement main and imports in app.py

---
+++ app.py
@@ -0,0 +1,11 @@
+import ipd_lab.common.loader as loader
+import ipd_lab.engines as engines
+import ipd_lab.strategies as strategies
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
